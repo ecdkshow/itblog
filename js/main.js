1b6ff5ffd454b947042de76eb7500037dd444b87
ecdkshow <ecdkshow@gmail.com>
2022-06-24 21:53:25 +0900

    js修正

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@ $(function(){
     $('.header').load('/js/header.html');
     $('.footer').load('/js/footer.html');
 
-// TOPに戻るボタン
-var topBtn = $('#page-top');
-topBtn.hide();
-//スクロールが100に達したらボタン表示
-$(window).scroll(function () {
-var w = $(window).width();
-if ($(this).scrollTop() > 100) {
-topBtn.fadeIn();
-} else {
-topBtn.fadeOut();
-}
-});
-//スムーススクロールでページトップへ
-topBtn.click(function () {
-$('body,html').animate({
-scrollTop: 0
-}, 500);
-return false;
-});
+    // TOPに戻るボタン
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        var w = $(window).width();
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スムーススクロールでページトップへ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 });
